7b6b794559463dfc4d400904b1cb17e12fd75945
buildgood <yiyiwu@Yiyis-MacBook-Pro.local>
2017-01-23 02:29:20 -0500

    Delete users array in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var user = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,12 +9,10 @@ router.get('/', function(req, res, next) {
 router.post('/chat', function(req, res) {
   var nickname = req.body.nickname;
   if(nickname) {
-    user.push(nickname);
     res.render('chat', {name: nickname});
   }else {
     res.redirect('/');
   }
-
 });
 
 module.exports = router;
